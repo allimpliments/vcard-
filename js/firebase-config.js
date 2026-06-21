89613002d75096c51044c790c1a52e54de1ed5d2
allimpliments <allimpliments@gmail.com>
2026-06-21 20:38:41 +0530

    Create firebase-config.js
    js/firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase configuration (बाद में बदलें)
+const firebaseConfig = {
+  apiKey: "AIza...",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "...",
+  appId: "..."
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
