1d7db078fa3e0bcf47c0ba76bdae2833e87391a4
allimpliments <allimpliments@gmail.com>
2026-06-21 20:56:19 +0530

    Update firebase-config.js
    update

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,10 @@
-// Firebase configuration (बाद में बदलें)
 const firebaseConfig = {
-  apiKey: "AIza...",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyAEOvOCqCbVAV1TUR8c8DchvCYahkQxV6c",
+  authDomain: "avatar-vcard-app.firebaseapp.com",
+  projectId: "avatar-vcard-app",
+  storageBucket: "avatar-vcard-app.firebasestorage.app",
+  messagingSenderId: "362969682654",
+  appId: "1:362969682654:web:87fbf7cdd7a76bbccf684f"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
